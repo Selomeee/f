20181f84b3e2837b8f434b823cd5d12f27195330
Selomeee <selomefiseha26@gmail.com>
2024-12-15 17:54:35 +0000

    Update mn.js

--- mn.js
+++ mn.js
@@ -1,6 +1,1 @@
-const menuIcon = document.getElementById('menu-icon');
-const navbar = document.querySelector('.navbar');
 
-menuIcon.addEventListener('click', () => {
-    navbar.classList.toggle('active');
-});
